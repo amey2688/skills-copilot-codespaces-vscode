da5a961ee0f8cb520d2cbbfe91292af546d7dad4
amey2688 <33552841+amey2688@users.noreply.github.com>
2024-01-14 13:34:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsDiv = document.getElementById("memberSkillsDiv");
+    var memberSkillsDivValue = memberSkillsDiv.options[memberSkillsDiv.selectedIndex].value;
+    var memberSkillsDivText = memberSkillsDiv.options[memberSkillsDiv.selectedIndex].text;
+    var memberSkillsDivDiv = document.getElementById("memberSkillsDivDiv");
+    var memberSkillsDivDivValue = memberSkillsDivDiv.options[memberSkillsDivDiv.selectedIndex].value;
+    var memberSkillsDivDivText = memberSkillsDivDiv.options[memberSkillsDivDiv.selectedIndex].text;
+    var memberSkillsDivDivDiv = document.getElementById("memberSkillsDivDivDiv");
+    var memberSkillsDivDivDivValue = memberSkillsDivDivDiv.options[memberSkillsDivDivDiv.selectedIndex].value;
+    var memberSkillsDivDivDivText = memberSkillsDivDivDiv.options[memberSkillsDivDivDiv.selectedIndex].text;
+    var memberSkillsDivDivDivDiv = document.getElementById("memberSkillsDivDivDivDiv");
+    var memberSkillsDivDivDivDivValue = memberSkillsDivDivDivDiv.options[memberSkillsDivDivDivDiv.selectedIndex].value;
+    var memberSkillsDivDivDivDivText = memberSkillsDivDivDivDiv.options[memberSkillsDivDivDivDiv.selectedIndex].text;
+    var memberSkillsDivDivDivDivDiv = document.getElementById("memberSkillsDivDivDivDivDiv");
+    var memberSkillsDivDivDivDivDivValue = memberSkillsDivDivDivDivDiv.options[memberSkillsDivDivDivDivDiv.selectedIndex].value;
+    var memberSkillsDivDivDivDivDivText = memberSkillsDivDivDivDivDiv.options[memberSkillsDivDivDivDivDiv.selectedIndex].text;
+    var memberSkillsDivDivDivDivDivDiv = document.getElementById("memberSkillsDivDivDivDivDivDiv");
+    var memberSkillsDivDivDivDivDivDivValue = memberSkillsDivDivDivDivDivDiv.options[memberSkillsDivDivDivDivDivDiv.selectedIndex].value;
+    var memberSkillsDivDivDivDivDivDivText = memberSkillsDivDivDivDivDivDiv.options[memberSkillsDivDivDivDiv
